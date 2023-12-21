6ee09b42194f69842b53ab28a3f6d4f15e1e4e0a
44jovi <kwong.jovi@gmail.com>
2023-12-21 11:04:18 +0000

    update(go_text_encrypter_go): encrypt()

--- go_text_encrypter.go
+++ go_text_encrypter.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
 
 func main() {
 	fmt.Println("WIP")
+}
+
+func encrypt(plaintext []byte, key []byte) ([]byte, error) {
+    // Create block cipher
+	block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    // Create GCM cipher
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    // Random nonce for each encryption
+    nonce := make([]byte, gcm.NonceSize())
+	// Verify nonce size is valid
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        return nil, err
+    }
+
+    // Encrypt the plaintext
+	// Reuse storage of 'plaintext' for ciphertext destination
+    ciphertext := gcm.Seal(plaintext[:0], nonce, plaintext, nil)
+    return ciphertext, nil
 }
